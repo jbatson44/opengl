2152a704e687aa628c50fa8f6c8a52e624bc1a72
jbatson44 <bat14009@byui.edu>
2020-03-09 11:11:22 -0600

    Add project files.

---
+++ opengl/main.cpp
@@ -0,0 +1,76 @@
+#include <glad/glad.h> 
+#include <GLFW\glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // glViewport(lowerleft corner x, y, width, height
+    // We could actually set the viewport dimensions at values 
+    // smaller than GLFW's dimensions; then all the OpenGL 
+    // rendering would be displayed in a smaller window and 
+    // we could for example display other elements outside 
+    // the OpenGL viewport.
+    glViewport(0, 0, 800, 600);
+
+    // set function that will allow us to resize the window
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        processInput(window);
+
+        // commands
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check and call events and swap the buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+/*********************************************************************
+* resize opengl viewport
+*********************************************************************/
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
